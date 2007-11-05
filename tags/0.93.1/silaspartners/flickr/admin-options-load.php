780e3dcee640be77c3c3f347f853d3ceba502345
joetan54 <joetan54@cd80fd39-c519-0410-8bc8-5f36e4e48a33>
2007-11-05 03:53:47 +0000

    0.93 release - update for wp 2.3, and updates to caching logic

---
+++ tags/0.93.1/silaspartners/flickr/admin-options-load.php
@@ -0,0 +1,48 @@
+<?php
+$tmpPath = '/../../../..';
+if (!file_exists(dirname(__FILE__).$tmpPath.'/wp-config.php')) {
+$tmpPath = '/../../../../..';
+    if (!file_exists(dirname(__FILE__).$tmpPath.'/wp-config.php')) {
+        echo "Error: wp-config.php not found";
+        exit;
+    }
+}
+if (!isset($_GET['view']) ) {
+    exit;
+}
+require_once(dirname(__FILE__).$tmpPath.'/wp-config.php');
+//require_once(dirname(__FILE__).$tmpPath.'/wp-admin/admin-functions.php');
+//require_once(dirname(__FILE__).$tmpPath.'/wp-admin/admin-db.php');
+
+get_currentuserinfo();
+//print_r(wp_get_current_user());
+//if ( !current_user_can('manage_categories') )
+//	die('Insufficient priviledges');
+
+function get_out_now() { exit; }
+
+add_action('shutdown', 'get_out_now', -1);
+
+require_once(dirname(__FILE__).'/lib.flickr.php');
+$flickr = new SilasFlickr();
+$auth_token  = get_option('silas_flickr_token');
+$hideAlbums  = get_option('silas_flickr_hidealbums');
+$hideGroups  = get_option('silas_flickr_hidegroups');
+$groupOrder  = get_option('silas_flickr_grouporder');
+$albumOrder  = get_option('silas_flickr_albumorder');
+        
+$flickr->setToken($auth_token);
+$flickr->setOption(array(
+    'hidePrivatePhotos' => get_option('silas_flickr_hideprivate'),
+));
+$user = $flickr->auth_checkToken();
+$flickr->setUser($user);
+
+if ($_GET['view'] == 'albums') { // load albums
+    if (!is_array($hideAlbums)) $hideAlbums = array();
+    include(dirname(__FILE__).'/admin-options-albums.html');
+} elseif ($_GET['view'] == 'groups') { // load groups
+    if (!is_array($hideGroups)) $hideGroups = array();
+    include(dirname(__FILE__).'/admin-options-groups.html');
+}
+?>
