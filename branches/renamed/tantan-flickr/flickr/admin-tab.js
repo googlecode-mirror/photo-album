c5d608276262e1ed33551936f09d061d59357afc
joetan54 <joetan54@cd80fd39-c519-0410-8bc8-5f36e4e48a33>
2008-04-30 04:29:40 +0000

    testing renaming silaspartners to tantan-flickr

---
+++ branches/renamed/tantan-flickr/flickr/admin-tab.js
@@ -0,0 +1,48 @@
+var lastPhoto = false;
+function tantan_showOptions(id) {
+    if (lastPhoto) tantan_hideOptions(lastPhoto)
+    lastPhoto = id
+
+    var div = document.getElementById('options-'+id)
+    if (div) div.style.display='block';
+    return false;
+}
+function tantan_hideOptions(id) {
+    var div = document.getElementById('options-'+id)
+    if (div) div.style.display='none';
+    
+    var e = window.event;
+	if (e) {
+        e.cancelBubble = true;
+    	if (e.stopPropagation) e.stopPropagation();
+    }
+    return false;
+}
+function tantan_addPhoto(photoUrl, sourceUrl, width, height, title, size) {
+	var h = tantan_makePhotoHTML(photoUrl, sourceUrl, width, height, title, size);
+	if (typeof top.send_to_editor == 'function') {
+		top.send_to_editor(h);
+	} else {
+	    var win = window.opener ? window.opener : window.dialogArguments;
+		if ( !win ) win = top;
+		tinyMCE = win.tinyMCE;
+		if ( typeof tinyMCE != 'undefined' && tinyMCE.getInstanceById('content') ) {
+			tinyMCE.selectedInstance.getWin().focus();
+			tinyMCE.execCommand('mceInsertContent', false, h);
+		} else if (win.edInsertContent) win.edInsertContent(win.edCanvas, h);
+	}
+	if (typeof top.tb_remove == 'function' && document.getElementById('closewindowcheck') && document.getElementById('closewindowcheck').checked) 
+		top.tb_remove();
+
+	return false;
+}
+function tantan_makePhotoHTML(photoUrl, sourceUrl, width, height, title, size) { 
+	return '<a href="'+photoUrl+'" class="tt-flickr'+(size ? (' tt-flickr-'+size) : '')+'">' +
+		'<img src="'+sourceUrl+'" alt="'+title+'" width="'+width+'" height="'+height+'" border="0" />' +
+		'</a> ';
+}
+function tantan_addShortCode(attribs) {
+	top.send_to_editor('[flickr'+(attribs ? (' '+attribs) : '')+']');
+	if (typeof top.tb_remove == 'function') 
+		top.tb_remove();
+}
