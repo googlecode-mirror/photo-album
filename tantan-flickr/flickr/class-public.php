56eeb9d4f9af84a7d5e3f1bcd2e88cc30114b578
joetan54 <joetan54@cd80fd39-c519-0410-8bc8-5f36e4e48a33>
2008-09-04 20:19:41 +0000

    fix xhtml compliance, issue #98

--- tantan-flickr/flickr/class-public.php
+++ tantan-flickr/flickr/class-public.php
@@ -477,7 +477,7 @@ class TanTanFlickrPlugin {
     function rss_feed() {
         $user = get_option('silas_flickr_user');
         if ($this->request['album']) {
-            echo '<link rel="alternate" type="application/atom+xml" title="Flickr Album Feed" href="http://api.flickr.com/services/feeds/photoset.gne?set='.$this->request['album'].'&nsid='.$user['user']['nsid'].'" />';
+            echo '<link rel="alternate" type="application/atom+xml" title="Flickr Album Feed" href="http://api.flickr.com/services/feeds/photoset.gne?set='.$this->request['album'].'&amp;nsid='.$user['user']['nsid'].'" />';
         }
         if ($this->request['group']) {
             echo '<link rel="alternate" type="application/atom+xml" title="Flickr Group Feed" href="http://api.flickr.com/services/feeds/groups_discuss.gne?id='.$this->request['group'].'" />';
